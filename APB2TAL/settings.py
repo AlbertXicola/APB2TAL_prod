923d348e51987fdc0b235ced036f0c415838d2b2
alber@AlbertXS <albertxicola04@hotmail.com>
2024-05-27 00:53:37 +0200

    test

--- APB2TAL/settings.py
+++ APB2TAL/settings.py
@@ -133,10 +133,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#if not DEBUG:
+#    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_URL = '/signin'
 
